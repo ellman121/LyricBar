59a8a45e9ebb883779f020906b23351728a4d8f5
Elliott Rarden <617942+ellman121@users.noreply.github.com>
2018-01-02 11:20:36 +0100

    Running in menu bar now

--- LyricBar/AppDelegate.swift
+++ LyricBar/AppDelegate.swift
@@ -11,16 +11,23 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-
+    var statusBarItem: NSStatusItem? = nil
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
+        
+        statusBarItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.squareLength)
+        if let button = statusBarItem?.button {
+            button.image = NSImage(named:NSImage.Name("L"))
+            button.action = #selector(p(_:))
+        }
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
 
-
+    @objc func p(_ sender: Any?) {
+        print("hello world")
+    }
 }
-
