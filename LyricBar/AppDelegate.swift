fa585e17b9ea6863aa33083a8a26ab6110ac68f3
Elliott Rarden <617942+ellman121@users.noreply.github.com>
2018-01-02 10:55:32 +0100

    Initial Commit

---
+++ LyricBar/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  LyricBar
+//
+//  Created by Elliott Rarden on 02.01.18.
+//  Copyright © 2018 Elliott Rarden. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ LyricBar/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  LyricBar
+//
+//  Created by Elliott Rarden on 02.01.18.
+//  Copyright © 2018 Elliott Rarden. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
